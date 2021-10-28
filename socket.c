5b2b449a4e949da0ee064b7213068d590649f4ac
hey-kader <kaderarnold@icloud.com>
2021-10-28 02:40:05 -0700

    put error function and header inclusions into a local header file titled common.h

--- socket.c
+++ socket.c
@@ -1,27 +1,4 @@
-#include <stdio.h>
-#include <signal.h>
-#include <unistd.h>
-#include <arpa/inet.h>
-#include <stdlib.h>
-#include <sys/socket.h>
-#include <sys/types.h>
-#include <netinet/in.h>
-#include <string.h>
-#include <netdb.h>
-#include <sys/time.h>
-#include <fcntl.h>
-#include <errno.h>
-#include <sys/ioctl.h>
-#include <stdarg.h>
-
-#define MAX 255
-
-#define PORT 8080 
-#define IP "66.85.133.188"
-
-void error (void) {
-    printf ("error!\n");
-}
+#include "common.h"
 
 int main (int argc, char *argv[]) {
 
